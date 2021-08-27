3acb008d95659eae9dd8a0d5d355f7717ac862b9
Lara <lara@uwu.is>
2021-08-27 16:36:33 +0200

    Make the polybar launch script more stable

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
 
+# Check prerequisites
+for i in pgrep polybar whoami xrandr; do
+    if [ ! "$(command -v $i)" ]; then
+        echo "error: prerequisite $i not found in PATH" >&2
+        exit 1
+    fi
+done
+
 # Terminate already running bar instances
-killall -u $(whoami) -q polybar
+kill $(pgrep -u "$(whoami)" polybar)
 
 # Wait until the processes have been shut down
-while pgrep -u $(whoami) -x polybar >/dev/null; do sleep 1; done
+while pgrep -u "$(whoami)" polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload default &
+    MONITOR=$m setsid polybar -l error --reload default &
   done
 else
-  polybar --reload default &
+  setsid polybar -l error --reload default &
 fi
-
 
 echo "Bars launched."
